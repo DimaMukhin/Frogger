18a5297b268255ca190957aa3522ab868ff32207
DimaMukhin <dima9348@gmail.com>
2017-06-11 00:09:33 -0500

    Formatting and readability

--- frogger.c
+++ frogger.c
@@ -1,3 +1,8 @@
+/*
+ * FILE     : frogger.c
+ * REMARKS  : Contains main game logic, thread initialization, etc.
+ */
+
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -50,233 +55,275 @@ char *GAME_BOARD[] = {
 "",
 "" };
 
-/* function prototypes */
-void *draw();
-void *keyboard();
-void *upkeep();
-void initializeThreads();
-void gameOverWait();
-void joinThreads();
-void cleanDeadLogs();
-void cleanLogRows();
-void drawGameBoard();
-int gameWon();
+/* private function prototypes */
+
+void *draw();               // draw and refresh thread
+void *keyboard();           // keyboard thread
+void *upkeep();             // upkeep thread
+void initializeThreads();   // initialize all threads
+void gameOverWait();        // wait for game to be over (CV)
+void joinThreads();         // join all threads
+void cleanDeadLogs();       // cleanup of dead logs
+void cleanLogRows();        // general cleanup of all log rows
+void drawGameBoard();       // draw the game board
+int gameWon();              // was game won?
 
 /* private variables */
-char *gameOverMessage;
+
+char *gameOverMessage;      // message to display once the game is over (win/lose)
 
 /* private pthreads */
-pthread_t drawThread;
-pthread_t keyboardThread;
-pthread_t playerUpdateThread;
-pthread_t upkeepThread;
-pthread_t logRowThread[NUM_ROWS];
+
+pthread_t drawThread;               // draw/refresh thread
+pthread_t keyboardThread;           // keyboard input thread
+pthread_t playerUpdateThread;       // player logic thread
+pthread_t upkeepThread;             // upkeep (cleanup) thread
+pthread_t logRowThread[NUM_ROWS];   // log row thread
 
 /*** public functions ***/
 
+/*-----------------------------------------------------------------------------runFrogger
+ * main game thread,
+ * controls initialization of threads, and end game cleanup
+ */
 void *runFrogger()
 {
-	// initialize all game globals
-	initializeGameGlobals();
-		
-	// initialize all threads
-	initializeThreads();
-	
-	// wait for game over signal
-	gameOverWait();
-	
-	// clean up
-	joinThreads();
-	cleanLogRows();
-
-	// print final message and exit
-	consoleFinish();
-	
-	pthread_mutex_lock(&drawMutex);
-	putBanner(gameOverMessage);
-	consoleRefresh();
-	pthread_mutex_unlock(&drawMutex);
-	
-	finalKeypress();
-	pthread_exit(NULL);
+    initializeGameGlobals();
+        
+    initializeThreads();
+    
+    gameOverWait();
+    
+    // Wait is done... (game is over)
+    
+    joinThreads();
+    cleanLogRows();
+    consoleFinish();
+    
+    // draw end game message
+    pthread_mutex_lock(&drawMutex);
+    putBanner(gameOverMessage);
+    consoleRefresh();
+    pthread_mutex_unlock(&drawMutex);
+    
+    finalKeypress();
+    pthread_exit(NULL);
 }
 
-
+/*-------------------------------------------------------------------------drawLives
+ * Draws remaining life on screen
+ */
 void drawLives()
 {
-	char life;
-	sprintf(&life, "%d", getLives());
-	
-	pthread_mutex_lock(&drawMutex);
-	putString(&life, 0, LIFE_COL, LIFE_ROW);
-	pthread_mutex_unlock(&drawMutex);
+    char life;
+    sprintf(&life, "%d", getLives());
+    
+    pthread_mutex_lock(&drawMutex);
+    putString(&life, 0, LIFE_COL, LIFE_ROW);
+    pthread_mutex_unlock(&drawMutex);
 }
 
+/*---------------------------------------------------------------------checkGameOver
+ * Check if the game is over and handle end of game
+ */
 void checkGameOver()
 {
-	if (getLives() == 0)
-	{
-		gameOverMessage = MSG_LOST;
-		gameOver = 1;
-	}	
-	else if (gameWon())
-	{
-		gameOverMessage = MSG_WON;
-		gameOver = 1;
-	}
-		
-	if (gameOver)
-	{
-		pthread_mutex_lock(&gameOverMutex);
-		pthread_cond_signal(&gameOverCV);
-		pthread_mutex_unlock(&gameOverMutex);
-	}
+    if (getLives() == 0)
+    {
+        gameOverMessage = MSG_LOST;
+        gameOver = 1;
+    }   
+    else if (gameWon())
+    {
+        gameOverMessage = MSG_WON;
+        gameOver = 1;
+    }
+        
+    if (gameOver)
+    {
+        pthread_mutex_lock(&gameOverMutex);
+        pthread_cond_signal(&gameOverCV);
+        pthread_mutex_unlock(&gameOverMutex);
+    }
 }
 
 /*** private functions ***/
 
+/*------------------------------------------------------------------------------draw
+ * Refresh and draw of different components
+ */
 void *draw()
 {
-	consoleInit(GAME_ROWS, GAME_COLS, GAME_BOARD);
-	drawLives();
-	
-	drawGameBoard();
-	while (!gameOver)
-	{
-		sleepTicks(DEFAULT_SLEEP_TICKS);
-		
-		for (int i = 0; i < NUM_ROWS; i++)
-			drawLogRow(logRows[i]);
+    consoleInit(GAME_ROWS, GAME_COLS, GAME_BOARD);
+    drawLives();
+    
+    drawGameBoard();
+    while (!gameOver)
+    {
+        sleepTicks(DEFAULT_SLEEP_TICKS);
+        
+        for (int i = 0; i < NUM_ROWS; i++)
+            drawLogRow(logRows[i]);
 
-		drawPlayer();
-		
-		pthread_mutex_lock(&drawMutex);
-		consoleRefresh();
-		pthread_mutex_unlock(&drawMutex);
-	}
-	pthread_exit(NULL);
+        drawPlayer();
+        
+        pthread_mutex_lock(&drawMutex);
+        consoleRefresh();
+        pthread_mutex_unlock(&drawMutex);
+    }
+    pthread_exit(NULL);
 }
 
+/*--------------------------------------------------------------------------keyboard
+ * Handle keyboard input
+ */
 void *keyboard()
 {
-	fd_set set;
-	int key;
-	while (!gameOver)
-	{
-		sleepTicks(DEFAULT_SLEEP_TICKS);
-		
-		FD_ZERO(&set);
-		FD_SET(STDIN_FILENO, &set);
+    fd_set set;
+    int key;
+    while (!gameOver)
+    {
+        sleepTicks(DEFAULT_SLEEP_TICKS);
+        
+        /* timeout logic for joining the thread at end of game */
+        FD_ZERO(&set);
+        FD_SET(STDIN_FILENO, &set);
         struct timespec timeout = getTimeout(1); /* duration of one tick */
         int ret = pselect(FD_SETSIZE, &set, NULL, NULL, &timeout, NULL);
-		if (ret == -1) 
-		{ 
-			perror("pselect error"); 
-			exit(EXIT_FAILURE);
-		}
-		
-		if (ret != 0)
-		{
-			key = getchar();	
-			if (key == KEY_W)
-				movePlayerUp();
-			else if (key == KEY_A)
-				movePlayerLeft();
-			else if (key == KEY_S)
-				movePlayerDown();
-			else if (key == KEY_D)
-				movePlayerRight();
-		}
-		
-	}
-	pthread_exit(NULL);
+        if (ret == -1) 
+        { 
+            perror("pselect error"); 
+            exit(EXIT_FAILURE);
+        }
+        
+        /* keyboard input logic */
+        if (ret != 0)
+        {
+            key = getchar();    
+            if (key == KEY_W)
+                movePlayerUp();
+            else if (key == KEY_A)
+                movePlayerLeft();
+            else if (key == KEY_S)
+                movePlayerDown();
+            else if (key == KEY_D)
+                movePlayerRight();
+        }
+        
+    }
+    pthread_exit(NULL);
 }
 
+/*----------------------------------------------------------------------------upkeep
+ * Upkeep (log cleanup)
+ */
 void *upkeep()
 {
-	while (!gameOver)
-	{
-		sleepTicks(DEFAULT_SLEEP_TICKS);
-		cleanDeadLogs();
-	}
-	
-	pthread_exit(NULL);
+    while (!gameOver)
+    {
+        sleepTicks(DEFAULT_SLEEP_TICKS);
+        cleanDeadLogs();
+    }
+    
+    pthread_exit(NULL);
 }
 
+/*-----------------------------------------------------------------initializeThreads
+ * initialize all threads (except for log threads)
+ */
 void initializeThreads()
 {
-	createThread(&drawThread, draw, NULL);
-	createThread(&keyboardThread, keyboard, NULL);
-	createThread(&upkeepThread, upkeep, NULL);
-	createThread(&playerUpdateThread, playerUpdate, NULL);
-	for (int i = 0; i < NUM_ROWS; i++)
-	{
-		logRows[i] = createLogRow(i);
-		createThread(&logRowThread[i], logRowUpdate, logRows[i]);
-	}
+    createThread(&drawThread, draw, NULL);
+    createThread(&keyboardThread, keyboard, NULL);
+    createThread(&upkeepThread, upkeep, NULL);
+    createThread(&playerUpdateThread, playerUpdate, NULL);
+    for (int i = 0; i < NUM_ROWS; i++)
+    {
+        logRows[i] = createLogRow(i);
+        createThread(&logRowThread[i], logRowUpdate, logRows[i]);
+    }
 }
 
+/*-----------------------------------------------------------------------joinThreads
+ * join all threads (except for log threads)
+ */
 void joinThreads()
 {
-	pthread_join(drawThread, NULL);
-	pthread_join(keyboardThread, NULL);
-	pthread_join(upkeepThread, NULL);
-	pthread_join(playerUpdateThread, NULL);
-	for (int i = 0; i < NUM_ROWS; i++)
-		pthread_join(logRowThread[i], NULL);
+    pthread_join(drawThread, NULL);
+    pthread_join(keyboardThread, NULL);
+    pthread_join(upkeepThread, NULL);
+    pthread_join(playerUpdateThread, NULL);
+    for (int i = 0; i < NUM_ROWS; i++)
+        pthread_join(logRowThread[i], NULL);
 }
 
+/*---------------------------------------------------------------------cleanDeadLogs
+ * free dead logs and remove them from the list
+ */
 void cleanDeadLogs()
 {
-	for (int i = 0; i < NUM_LOG_ROWS; i++)
-	{
-		LogRow *logRow = logRows[i];
-		LogList *logList = logRow->logs;
-		LogNode *curr = logList->top;
-		while (curr != NULL) 
-		{
-			Log *log = curr->log;
-			if (log != NULL && !log->alive)
-			{
-				cleanLog(log);
-			}
-			curr = curr->next;
-		}
-	}
+    for (int i = 0; i < NUM_LOG_ROWS; i++)
+    {
+        LogRow *logRow = logRows[i];
+        LogList *logList = logRow->logs;
+        LogNode *curr = logList->top;
+        while (curr != NULL) 
+        {
+            Log *log = curr->log;
+            if (log != NULL && !log->alive)
+            {
+                cleanLog(log);
+            }
+            curr = curr->next;
+        }
+    }
 }
 
+/*----------------------------------------------------------------------cleanLogRows
+ * clean of all data stractures: log list, logs, log rows etc.
+ * cleanup for end of game.
+ */
 void cleanLogRows()
 {
-	for (int i = 0; i < NUM_LOG_ROWS; i++)
-	{
-		cleanLogRow(logRows[i]);
-	}
+    for (int i = 0; i < NUM_LOG_ROWS; i++)
+    {
+        cleanLogRow(logRows[i]);
+    }
 }
 
+/*----------------------------------------------------------------------gameOverWait
+ * setup a condition variable and wait for an end of game signal
+ */
 void gameOverWait()
 {
-	pthread_mutex_lock(&gameOverMutex);
-	while (!gameOver)
-		pthread_cond_wait(&gameOverCV, &gameOverMutex);
-	pthread_mutex_unlock(&gameOverMutex);
+    pthread_mutex_lock(&gameOverMutex);
+    while (!gameOver)
+        pthread_cond_wait(&gameOverCV, &gameOverMutex);
+    pthread_mutex_unlock(&gameOverMutex);
 }
 
+/*---------------------------------------------------------------------drawGameBoard
+ * draw the board!
+ */
 void drawGameBoard()
 {
-	pthread_mutex_lock(&drawMutex);
-	consoleDrawImage(0, 0, GAME_BOARD, GAME_ROWS);
-	pthread_mutex_unlock(&drawMutex);
+    pthread_mutex_lock(&drawMutex);
+    consoleDrawImage(0, 0, GAME_BOARD, GAME_ROWS);
+    pthread_mutex_unlock(&drawMutex);
 }
 
+/*---------------------------------------------------------------------------gameWon
+ * check if game was won
+ */
 int gameWon()
 {
-	Home *home;
-	for (int i = 0; i < NUM_HOMES; i++)
-	{
-		home = getHomeIndex(i);
-		if (home->open)
-			return 0;
-	}
-	
-	return 1;
+    Home *home;
+    for (int i = 0; i < NUM_HOMES; i++)
+    {
+        home = getHomeIndex(i);
+        if (home->open)
+            return 0;
+    }
+    
+    return 1;
 }
--- main.c
+++ main.c
@@ -1,18 +1,31 @@
+/*
+ * NAME                     : Dima Mukhin
+ * STUDENT NUMBER           : 7773184
+ * COURSE                   : COMP 3430
+ * INSTRUCTOR               : Dr. Jim Young
+ * ASSIGNMENT               : 2
+ * Question                 : 1
+ * 
+ */
+
+/*
+ * FILE     : main.c
+ * REMARKS  : Contains main function for running the game
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "gameglobals.h"
 #include "frogger.h" 
+#include "threadwrappers.h"
 
+// Start Frogger thread and wait for the game to finish
 int main(int argc, char**argv) {
-	runFrogger();
-	
-	pthread_t froggerThread;
-	if (pthread_create(&froggerThread, NULL, runFrogger, NULL) == -1) 
-	{ 
-		perror("froggerThread"); 
-		exit(EXIT_FAILURE);
-	}
-	
-	pthread_join(froggerThread, NULL);
-	printf("done!\n");
+    runFrogger();
+    
+    pthread_t froggerThread;
+    createThread(&froggerThread, runFrogger, NULL);
+    
+    pthread_join(froggerThread, NULL);
 }
